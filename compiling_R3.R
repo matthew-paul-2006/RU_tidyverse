0071a7a2086e452fe7fcc6f3a8351e7339d62851
matthew-paul-2006 <matthew.paul.2006@gmail.com>
2019-12-10 18:34:51 -0500

    updated compiler and markdown

--- compiling_R3.R
+++ compiling_R3.R
@@ -1,7 +1,12 @@
 
-pathToPres <- "/Users/tcarroll/Projects/Software/teaching/tidyR"
-wkdDir <- "/Users/tcarroll/Projects/Software/teaching/tidyR/"
-library(rmarkdown)
+# pathToPres <- "/Users/tcarroll/Projects/Software/teaching/tidyR"
+# wkdDir <- "/Users/tcarroll/Projects/Software/teaching/tidyR/"
+
+
+pathToPres <- "/Users/mattpaul/Documents/Box Sync/RU/Teaching/teaching/tidyR"
+wkdDir <- "/Users/mattpaul/Documents/Box Sync/RU/Teaching/teaching/tidyR/"
+
+
 setwd(wkdDir)
 dir.create(file.path(wkdDir,"presentations","slides","imgs"),recursive = TRUE,showWarnings = FALSE)
 dir.create(file.path(wkdDir,"presentations","singlepage","imgs"),recursive = TRUE,showWarnings = FALSE)
@@ -24,19 +29,23 @@ file.copy(dir(file.path(pathToPres,"presRaw","imgs"),full.names = T),
           file.path(wkdDir,"presentations","slides","imgs"),recursive=TRUE)
 file.copy(dir(file.path(pathToPres,"presRaw","imgs"),full.names = T),
           file.path(wkdDir,"presentations","singlepage","imgs"),recursive=TRUE)
+
 #setwd("/Users/tcarroll/Projects/Software/Github/Intro_To_R_1Day_RU/r_course/presentations/slides/")
 #xaringan::inf_mr(cast_from = './presentations/slides/')
+
 for(f in filesToCompile){
   file.copy(f,file.path(wkdDir,"presentations","slides",basename(f)),overwrite=TRUE)
+  invisible(lapply(paste0('package:', names(sessionInfo()$otherPkgs)), detach, character.only=TRUE, unload=TRUE))
+  library(rmarkdown)
   render(file.path(wkdDir,"presentations","slides",basename(f)),output_format = "xaringan::moon_reader",knit_root_dir = getwd())
   invisible(lapply(paste0('package:', names(sessionInfo()$otherPkgs)), detach, character.only=TRUE, unload=TRUE))
   library(rmarkdown)
   render(file.path(wkdDir,"presentations","slides",basename(f)),output_format = "html_document",output_dir = file.path(wkdDir,"presentations","singlepage"),knit_root_dir = getwd())
 }
 
-require(rmarkdown)
-wkdDir <- "~/Projects/Software/teaching/tidyR/"
-filesToCompile <- dir(file.path(wkdDir,"presentations","slides"),pattern="*.Rmd$",full.names = T)
-
-render(file.path(wkdDir,"presentations","slides",basename(f)),output_format = "xaringan::moon_reader",knit_root_dir = getwd())
-render(file.path(wkdDir,"presentations","slides",basename(f)),output_format = "html_document",output_dir = file.path(wkdDir,"presentations","singlepage"),knit_root_dir = getwd())
+# require(rmarkdown)
+# wkdDir <- "~/Projects/Software/teaching/tidyR/"
+# filesToCompile <- dir(file.path(wkdDir,"presentations","slides"),pattern="*.Rmd$",full.names = T)
+# 
+# render(file.path(wkdDir,"presentations","slides",basename(f)),output_format = "xaringan::moon_reader",knit_root_dir = getwd())
+# render(file.path(wkdDir,"presentations","slides",basename(f)),output_format = "html_document",output_dir = file.path(wkdDir,"presentations","singlepage"),knit_root_dir = getwd())
